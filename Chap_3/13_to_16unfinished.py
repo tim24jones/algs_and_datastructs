6c009456b685d5ac67708dfd50cc199e38afdbf7
Tim Jones <tim.24.jones@gmail.com>
2017-08-30 23:51:51 -0400

    Add files via upload

---
+++ Chap_3/13_to_16unfinished.py
@@ -0,0 +1,109 @@
+class Node:
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
+
+class UnorderedList:
+
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        return '[',self.list_names(),']'
+
+    def list_names
+        list=self.head
+        current_item=self.head
+        while isEmpty(self)!=None:
+            current_node=Node(current_item)
+            current_node=current_node.getNext()
+            list=list+current_node
+            self.list_names=list
+            return list
+
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self,item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def length(self):
+        if isEmpty(self)==true:
+            return 0
+        else:
+            listlength=0
+            current_item=self.head
+            while isEmpty(self)!=None:
+                current_node=Node(current_item)
+                listlength=listlength+1
+                current_node=current_node.getNext()
+            return listlength
+                
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+        return count
+
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        if search(item)==False
+            print('item already removed')
+            return None
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+    def search(self,item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+        return found
+
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
