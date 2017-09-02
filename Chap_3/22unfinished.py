e051d390f3366fd8db828bd3e3cacd743ac88147
Tim Jones <tim.24.jones@gmail.com>
2017-09-01 20:05:51 -0400

    Add files via upload

---
+++ Chap_3/22unfinished.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
+
+class OrderedList:
+    def __init__(self):
+        self.head = None
+
+    def search(self,item):
+        current = self.head
+        found = False
+        stop = False
+        while current != None and not found and not stop:
+            if current.getData() == item:
+                found = True
+            else:
+                if current.getData() > item:
+                    stop = True
+                else:
+                    current = current.getNext()
+        return found
+
+    def add(self,item):
+        current = self.head
+        previous = None
+        stop = False
+        while current != None and not stop:
+            if current.getData() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+        temp = Node(item)
+        if previous == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(current)
+            previous.setNext(temp)
+
+class ordered_list_stack:
+    def __init__(self):
+        orderedlist(self)
+
+    def pop(self)
+        item=orderedlist.remove(self,orderedlist.size())
+        return item
+    def self.push(self,item)
+        orderedlist.append(self,item)
+    def self.peek(self)
+        item=orderedlist.getData()
+        return item
